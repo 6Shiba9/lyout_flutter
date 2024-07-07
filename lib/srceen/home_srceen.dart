79ffb37b5543023b14802ca8c216fb48c54281ff
Thanachai Yatar <cuter_1911_@hotmail.com>
2024-07-07 10:06:08 +0700

    addbox

--- lib/srceen/home_srceen.dart
+++ lib/srceen/home_srceen.dart
@@ -15,25 +15,86 @@ class _HomeSrceenState extends State<HomeSrceen> {
         title: const Text('Home'),
       ),
       body: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Container(
             color: Colors.red,
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Container(
-                  child: Text('1'),
+                Column(children: [
+                  Text(
+                    'The sae',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  Text("asdasd")
+                ]),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Row(
+                      children: [
+                        Icon(
+                          Icons.star,
+                          size: 14,
+                        ),
+                        Icon(
+                          Icons.star,
+                          size: 14,
+                        ),
+                        Icon(
+                          Icons.star,
+                          size: 14,
+                        ),
+                        Icon(
+                          Icons.star,
+                          size: 14,
+                        ),
+                        Icon(
+                          Icons.star,
+                          size: 14,
+                        ),
+                      ],
+                    ),
+                    Text("300 reviews")
+                  ],
                 ),
-                Container(
-                  child: Text('1'),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Column(
+                      children: [
+                        Icon(Icons.nat),
+                        Text("Test"),
+                        Text("test"),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Icon(Icons.arrow_back_rounded),
+                        Text("Test"),
+                        Text("test"),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Icon(Icons.turn_slight_left_outlined),
+                        Text("Test"),
+                        Text("test"),
+                      ],
+                    ),
+                  ],
                 ),
-                Container(child: Text('1')),
-                Container(child: Text('1')),
               ],
             ),
           ),
           Container(
             color: Colors.blue,
-            child: Image.asset('assets/images/sea.jpg'),
+            child: Image.asset(
+              'assets/images/sea.jpg',
+              height: 200,
+              width: 250,
+            ),
           ),
         ],
       ),
